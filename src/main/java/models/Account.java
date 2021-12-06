17ded2eabecd07f205ea23ce9324cc635fb99452
oumaima-kboubi <oumaima.kboubi@insat.u-carthage.tn>
2021-12-06 23:24:42 +0000

    initial commit: basic maven project + user account Model

---
+++ src/main/java/models/Account.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.Date;
+
+public class Account {
+    public  String firstName;
+    public String name;
+    public String email;
+    public String password;
+    public Date birthday;
+
+    public Account (String firstName, String name, String email, String password, Date birthday){
+        this.firstName=firstName;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.birthday=birthday;
+    }
+}
