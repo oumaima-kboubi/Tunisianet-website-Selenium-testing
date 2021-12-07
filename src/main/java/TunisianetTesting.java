46bbdcf3b632178eca758dcbb9aadcabce3c37a1
oumaima-kboubi <oumaima.kboubi@insat.u-carthage.tn>
2021-12-07 01:30:47 +0000

    Reconnexion au compte

--- src/main/java/TunisianetTesting.java
+++ src/main/java/TunisianetTesting.java
@@ -22,6 +22,7 @@ public class TunisianetTesting {
 
     //instance du l'exécuteur de Js
     JavascriptExecutor js;
+
     @Before
     public void initDriver() {
         //Utilisation d'un navigateur Opera
@@ -29,7 +30,7 @@ public class TunisianetTesting {
         driver = new OperaDriver();
 
         //initialisation de l'exécuteur Js
-        js=(JavascriptExecutor) driver;
+        js = (JavascriptExecutor) driver;
 
         //NB: Cette initialisation (time,TimeUnit) est "deprecated" et remplacée par (Duration)
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -55,14 +56,14 @@ public class TunisianetTesting {
 
         //TODO:Cliquer sur le bouton "connexion"
         Thread.sleep(1000);
-        WebElement connectionButton = driver.findElement(By.cssSelector(".user-down > li > a > span" ));
+        WebElement connectionButton = driver.findElement(By.cssSelector(".user-down > li > a > span"));
         connectionButton.click();
 
         //TODO:Cliquer sur "Pas de compte ? Créez-en un"
         Thread.sleep(1000);
         WebElement createAccountButton = driver.findElement(By.className("no-account"));
         //Verifier le bouton
-        Assert.assertEquals("Pas de compte ? Créez-en un",createAccountButton.findElement(By.cssSelector("*")).getText());
+        Assert.assertEquals("Pas de compte ? Créez-en un", createAccountButton.findElement(By.cssSelector("*")).getText());
         createAccountButton.click();
 
         //TODO:Choisir l'option "Mme"
@@ -73,18 +74,23 @@ public class TunisianetTesting {
         //TODO:Remplir le formulaire par les données du compte
         String format = "dd/MM/yyyy";
         SimpleDateFormat dateFormater = new SimpleDateFormat(format);
-        Account userAccount = new Account("oumaa","kbb","ouu@ouu.com","123abcABC",new Date(new Date().getTime() - TimeUnit.DAYS.toMillis(1) * 365 * 22)); //22ans
+        Account userAccount = new Account("oumab", "kba", "oua@oua.com", "123abcABC", new Date(new Date().getTime() - TimeUnit.DAYS.toMillis(1) * 365 * 22)); //22ans
 
         Thread.sleep(1000);
         List<WebElement> createAccountForm = driver.findElements(By.cssSelector("input.form-control"));
+        Thread.sleep(1000);
         createAccountForm.get(1).sendKeys(userAccount.firstName);
+        Thread.sleep(1000);
         createAccountForm.get(2).sendKeys(userAccount.name);
+        Thread.sleep(1000);
         createAccountForm.get(3).sendKeys(userAccount.email);
+        Thread.sleep(1000);
         createAccountForm.get(4).sendKeys(userAccount.password);
+        Thread.sleep(1000);
         createAccountForm.get(5).sendKeys(dateFormater.format(userAccount.birthday));
 
         //TODO:Basculer vers le bas
-        js.executeScript("window.scrollBy(0,250)","");
+        js.executeScript("window.scrollBy(0,250)", "");
 
         //TODO:Cliquer sur "Enregister"
         Thread.sleep(1000);
@@ -93,8 +99,37 @@ public class TunisianetTesting {
 
         //TODO:Se déconnecter du site
         Thread.sleep(1000);
+        userLoginDropdown=driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg"));
+        userLoginDropdown.click();
+        Thread.sleep(2000);
         WebElement logoutButton = driver.findElement(By.className("logout"));
         logoutButton.click();
+
+        //TODO:Se connecter de nouveau au compte récemment créé
+        //Cliquer sur l'icon user dans la navbar
+        Thread.sleep(1000);
+        userLoginDropdown= driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg"));
+        userLoginDropdown.click();
+
+        //Cliquer sur l'option connexion
+        Thread.sleep(1000);
+        connectionButton=driver.findElement(By.cssSelector(".user-down > li > a > span"));
+        connectionButton.click();
+
+        //Remplir le champs de l'email
+        Thread.sleep(1000);
+        WebElement emailField = driver.findElement(By.cssSelector(".form-group > div > input"));
+        emailField.sendKeys(userAccount.email);
+
+        //Remplir le champs de mots de passe
+        Thread.sleep(1000);
+        WebElement passwordField = driver.findElement(By.cssSelector(".form-group > div > div > input"));
+        passwordField.sendKeys(userAccount.password);
+
+        //Cliquer sur le bouton "Connexion"
+        Thread.sleep(1000);
+        WebElement loginButton = driver.findElement(By.id("submit-login"));
+        loginButton.click();
 
     }
 
